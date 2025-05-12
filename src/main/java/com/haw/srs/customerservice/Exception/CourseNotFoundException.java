0ea114c65953cc2814c58a28dff156840050fc36
Thore Nebel <thore.nebel@haw-hamburg.de>
2025-05-12 14:40:58 +0200

    Response Status Thore

--- src/main/java/com/haw/srs/customerservice/Exception/CourseNotFoundException.java
+++ src/main/java/com/haw/srs/customerservice/Exception/CourseNotFoundException.java
@@ -2,9 +2,12 @@ package com.haw.srs.customerservice.Exception;
 
 import lombok.EqualsAndHashCode;
 import lombok.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Value
 @EqualsAndHashCode(callSuper=false)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class CourseNotFoundException extends Exception {
 
     private final long courseNumber;
